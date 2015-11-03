d9ab92a6e320380450e4c96cdb0aac94b582a23b
Inndy <inndy.mail@gmail.com>
2015-11-03 11:33:17 +0800

    Remove ajax things

--- js/loadPage.js
+++ js/loadPage.js
@@ -1,6 +1,5 @@
 // include library
 require('./lib/dom.js');
-//var get = require('./lib/get.js');
 
 var pages = [
 	{url: './pages/about' , id: 'about_text' , btnid: 'about'},
@@ -14,11 +13,7 @@ var urlhash = window.location.hash.slice(8);
 var lock = false;
 for(var i=0 ; i<pages.length ; ++i){
 	var getPage = function(){
-		var container = pages[i].id;/*
-		get(pages[i].url , function(data){
-			Qid( container ).innerHTML = data;
-		});*/
-
+		var container = pages[i].id;
 		var trigger = pages[i].btnid;
 		addEvent(
 			document.getElementById( trigger ),
