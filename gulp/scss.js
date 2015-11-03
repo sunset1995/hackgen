f3be7ef7b1ab2738ec99f7ae818a987c31deb7ee
Inndy <inndy.mail@gmail.com>
2015-11-03 11:43:38 +0800

    @import is enough in scss, remove gulp-concat

--- gulp/scss.js
+++ gulp/scss.js
@@ -2,7 +2,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cssmin = require('gulp-cssmin');
-var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 
 
@@ -13,7 +12,6 @@ gulp.task('scss' , function(){
 	return gulp.src( sassPath.src )
 		.pipe(sass())
 		.on('error' , errorLog )
-		.pipe(concat('hackgen.css'))
 		.pipe(autoprefixer(['last 10 version']))
 		.pipe(cssmin())
 		.pipe(gulp.dest( sassPath.dst ));
