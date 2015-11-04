6db8f6b509978eca33f75548a798aa4b1aedd8e5
Inndy <inndy.mail@gmail.com>
2015-11-04 14:32:00 +0800

    Do not minify css under debug mode

--- gulp/scss.js
+++ gulp/scss.js
@@ -4,15 +4,22 @@ var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
 
-var sassPath = require('./_config.js').sass;
+var config = require('./_config.js');
+var sassPath = config.sass;
 var errorLog = require('./_errorLog.js').errorLog;
 
-gulp.task('scss' , function(){
+function compile_scss(output_style) {
+    output_style = output_style || 'compressed';
 	return gulp.src( sassPath.src )
 		.pipe(sass({
-			outputStyle: 'compressed'
+			outputStyle: output_style
 		}))
 		.on('error' , errorLog )
 		.pipe(autoprefixer(['last 10 version']))
 		.pipe(gulp.dest( sassPath.dst ));
-});
+}
+
+gulp.task('scss' , compile_scss);
+gulp.task('scss-debug', function () {
+    return compile_scss('nested');
+})
